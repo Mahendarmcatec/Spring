a7a69c8bc97553c49c627f8ced96552ed62e703c
Mahendarmcatec <mahendar.se@gmail.com>
2017-12-11 05:57:24 +0530

    embed db
    cg

---
+++ SpringEmbededDB/src/main/java/com/spring/embededdb/HomeController.java
@@ -0,0 +1,44 @@
+package com.spring.embededdb;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mkyong.dao.UserDao;
+import com.mkyong.model.User;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	UserDao userDao;
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		List<User> users = userDao.findAll();
+
+		System.out.println(users);
+
+		model.addAttribute("user", users);
+
+		return "welcome";
+	}
+
+}
---
+++ SpringEmbededDB/src/main/resources/db/sql/create-address.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Address (
+  id         INTEGER PRIMARY KEY,
+  address1 VARCHAR(30),
+  address2  VARCHAR(50)
+);
